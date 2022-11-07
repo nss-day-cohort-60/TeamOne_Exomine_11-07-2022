26f39ffe3c242f7f89d3287136f568951dab6b03
Nick Valdez <nicholastvaldez@gmail.com>
2022-11-07 14:01:34 -0600

    Add non-functioning dropdown list of governors

---
+++ scripts/governors.js
@@ -0,0 +1,34 @@
+/* Need getGovernors() function
+Need setGovernors() function 
+ */
+
+//import governor list 
+//import set governor
+import { getGovernors } from "./database.js"
+
+let governors = getGovernors()
+
+
+export const Governors = () => {
+    let html = "<p>Choose a governor</p>"
+
+    html += '<select id="governors">'
+    html += '<option value="0">Choose a governor...</option>'
+
+    for (const governor of governors) {
+        html += `
+        <option value="${governor.id}">${governor.name}</option>`
+    }
+
+    html += "</select>"
+}
+
+document.addEventListener(
+    "change",
+    (changeEvent) => {
+        if (changeEvent.target.id === "governors") {
+            setGovernors(pasrseInt(changeEvent.target.value))
+        }
+    }
+
+)
