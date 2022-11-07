a53f005701dcdcd79375dbf6d002bc1bf1c322ad
Vanessa Spear <vanessavspear@gmail.com>
2022-11-07 13:29:07 -0600

    added get/fetch functions to database.js module

--- scripts/database.js
+++ scripts/database.js
@@ -1,20 +1,124 @@
-const database = {
-    transientState: {}
+const applicationState = {
+    governors: [],
+    colonies: [],
+    facilities: [], 
+    minerals: [],
+    facilityMinerals: [],
+    orders: []
 }
 
-export const setFacility = (facilityId) => {
-    database.transientState.selectedFacility = facilityId
-    document.dispatchEvent( new CustomEvent("stateChanged") )
+const API = "http://localhost:8088"
+
+export const fetchGovernors = () => {
+    return fetch(`${API}/governors`)
+        .then(response => response.json())
+        .then(
+            (data) => {
+                applicationState.governors = data
+            }
+        )
+}
+
+export const getGovernors = () => {
+    return applicationState.governors.map(gov => ({...gov}))
+}
+
+export const fetchColonies = () => {
+    return fetch(`${API}/colonies`)
+        .then(response => response.json())
+        .then(
+            (data) => {
+                applicationState.colonies = data
+            }
+        )
+}
+
+export const getColonies = () => {
+    return applicationState.colonies.map(col => ({...col}))
+}
+
+export const fetchFacilities = () => {
+    return fetch(`${API}/facilities`)
+        .then(response => response.json())
+        .then(
+            (data) => {
+                applicationState.facilities = data
+            }
+        )
 }
 
 export const getFacilities = () => {
-    return database.facilities.map(f => ({...f}))
+    return applicationState.facilities.map(fac => ({...fac}))
 }
 
-export const purchaseMineral = () => {
+export const fetchMinerals = () => {
+    return fetch(`${API}/minerals`)
+        .then(response => response.json())
+        .then(
+            (data) => {
+                applicationState.minerals = data
+            }
+        )
+}
 
-        // Broadcast custom event to entire documement so that the
-        // application can re-render and update state
-        document.dispatchEvent( new CustomEvent("stateChanged") )
+export const getMinerals = () => {
+    return applicationState.minerals.map(min => ({...min}))
+}
+
+export const fetchFacilityMinerals = () => {
+    return fetch(`${API}/facilityMinerals`)
+        .then(response => response.json())
+        .then(
+            (data) => {
+                applicationState.facilityMinerals = data
+            }
+        )
+}
+
+export const getFacilityMinerals = () => {
+    return applicationState.facilityMinerals.map(facMin => ({...facMin}))
+}
+
+export const fetchColonyGovernors = () => {
+    return fetch(`${API}/colonyGovernors`)
+        .then(response => response.json())
+        .then(
+            (data) => {
+                applicationState.colonyGovernors = data
+            }
+        )
+}
+
+export const getColonyGovernors = () => {
+    return applicationState.colonyGovernors.map(colGov => ({...colGov}))
+}
+
+export const fetchOrders = () => {
+    return fetch(`${API}/orders`)
+        .then(response => response.json())
+        .then(
+            (data) => {
+                applicationState.orders = data
+            }
+        )
+}
+
+export const getOrders = () => {
+    return applicationState.orders.map(ord => ({...ord}))
+}
+
+export const completeOrder = (orderObj) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(orderObj)
     }
+
+    return fetch(`${API}/orders`, fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+            document.querySelector("#container").dispatchEvent(new CustomEvent("stateChanged"))
+        })
 }
